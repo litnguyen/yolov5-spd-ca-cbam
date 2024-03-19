c48040518fae8d5fc84609499f9958815b6f2ee9
daocongtuyen2x <daocongtuyen2x@gmail.com>
2024-03-19 23:02:02 +0700

    update

--- train.py
+++ train.py
@@ -384,10 +384,9 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
             # Backward
             with torch.autograd.detect_anomaly():
                 torch.use_deterministic_algorithms(False)
-                # scaler.scale(loss).backward()
-                loss.backward()
+                scaler.scale(loss).backward()
+                # loss.backward()
                 torch.use_deterministic_algorithms(True)
-            loss.backward()
 
             # Optimize - https://pytorch.org/docs/master/notes/amp_examples.html
             if ni - last_opt_step >= accumulate:
